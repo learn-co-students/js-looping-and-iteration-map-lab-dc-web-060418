28d54d552ef63153f9f2fe6e5c0fcdab9ac708ab
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-07-15 23:55:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+    return drivers.map(
+        function(driver) {
+            return driver.toLowerCase();
+        }
+    );
+}
+
+
+function nameToAttributes(drivers) {
+    return drivers.map(
+        function(driverName) {
+            const names = driverName.split(" ");
+            return {firstName: names[0], lastName: names[1]};
+        }
+    );
+}
+
+function attributesToPhrase(driverArray) {
+    return driverArray.map(
+        function(driverObject) {
+            return `${driverObject.name} is from ${driverObject.hometown}`;
+        }
+    );
+}
